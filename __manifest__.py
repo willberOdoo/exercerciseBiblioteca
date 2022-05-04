c9030274725b3896b553d292c82d248fc59e2831
“ <willrr.payall.2021@gmail.com>
2022-05-04 13:25:33 -0400

    [ADD] folder basic excercise biblioteca

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Biblioteca',
+
+    'summary': """ Training exercise""",
+
+    'description': """ 
+        Academy Module ti manage Training:
+        -excercise practic
+    """,
+    'autor': 'Payall',
+
+    'developer': 'Willber R.R',
+
+    'website': 'htps://www.odoo.com',
+
+    'category': 'Training',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+
+    ],
+
+    'demo': [
+    ],
+}
